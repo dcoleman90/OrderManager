40a2e735465977dd51e668f6f3dcfcfa736af008
dcoleman90 <dcolem12@my.westga.edu>
2020-12-08 23:48:37 -0500

    Initial code

---
+++ API/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public OrdersController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            return await _context.Orders.FindAsync(id);
+        }
+    }
+}
---
+++ API/Entities/Order.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string OrderName { get; set; }
+    }
+}
