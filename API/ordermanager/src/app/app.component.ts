a36c0c41040955f54a7e301eab071948cd5a203b
dcoleman90 <dcolem12@my.westga.edu>
2020-12-09 01:06:34 -0500

    Added Angular 10 Successfully fetched data into Angular app

---
+++ API/ordermanager/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'Orders';
+  orders: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.http.get('https://localhost:5001/api/orders').subscribe(
+      (response) => {
+        this.orders = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  returnOrders() {
+    console.log('TRY AGAINthis.orders');
+    console.log(this.orders);
+  }
+}
